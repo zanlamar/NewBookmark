99dd552da1858f8cc860fc7555e8cb1d59823886
Ana Laf <zanlamar@gmail.com>
2025-09-16 10:19:24 +0200

    Add tab-button and featuresbanner components kick off

---
+++ src/app/components/atoms/tab-button/tab-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-button',
+  imports: [],
+  templateUrl: './tab-button.html',
+  styleUrl: './tab-button.css'
+})
+export class TabButton {
+
+}
---
+++ src/app/components/molecules/featuresbanner/featuresbanner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featuresbanner',
+  imports: [],
+  templateUrl: './featuresbanner.html',
+  styleUrl: './featuresbanner.css'
+})
+export class Featuresbanner {
+
+}
