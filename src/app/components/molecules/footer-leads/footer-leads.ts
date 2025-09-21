27dca16f0883b0a1d04530f2cb1ce623309e4698
Ana Laf <zanlamar@gmail.com>
2025-09-21 19:42:56 +0200

    Footer mobile ok

--- src/app/components/molecules/footer-leads/footer-leads.ts
+++ src/app/components/molecules/footer-leads/footer-leads.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Button } from '../../atoms/button/button';
 
 @Component({
   selector: 'app-footer-leads',
-  imports: [],
+  imports: [Button],
   templateUrl: './footer-leads.html',
   styleUrl: './footer-leads.css',
   standalone: true,
