2d64e3a9c536c691f024fec07f86e81231889629
Ana Laf <zanlamar@gmail.com>
2025-09-23 17:43:22 +0200

    final gitignore

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component, signal } from '@angular/core';
 
 import { FeaturesSection } from "./components/organisms/features-section/features-section";
-import { DesktopHeaderSection } from "./components/organisms/desktop-header-section/desktop-header-section";
 import { ExtensionsSection } from "./components/organisms/extensions-section/extensions-section";
 import { FooterSection } from './components/organisms/footer-section/footer-section';
 import { FirstSection } from "./components/organisms/first-section/first-section";
+import { DesktopHeaderSection } from "./components/organisms/desktop-header-section/desktop-header-section";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FeaturesSection, DesktopHeaderSection, ExtensionsSection, FooterSection, FirstSection],
+  imports: [FeaturesSection, ExtensionsSection, FooterSection, FirstSection, DesktopHeaderSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
